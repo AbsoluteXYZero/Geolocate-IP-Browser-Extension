5adb1de0fc9568dd7d4e7d2e51f2a5ae8da49932
Zero <183767710+AbsoluteXYZero@users.noreply.github.com>
2026-04-01 17:45:32 -0400

    Enhance geo-location tracking and title updates
    Added currentIPv4 and currentIPv6 tracking, updated title with IP addresses, and improved fetchGeoLocation logic.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@ let latestGeoLocation = null;
 let latestGeoLocation6 = null;
 const lp = new LocalStorageProvider();
 let ipv4Error = false;
-const countriesSupported = ['AD', 'BA', 'BY', 'CV', 'ET', 'GN', 'IM', 'KR', 'MD', 'MW', 'PA', 'RU', 'ST', 'TT', 'WS', 'AE', 'BB', 'BZ', 'CW', 'EU', 'GQ', 'IN', 'KW', 'ME', 'MX', 'PE', 'RW', 'SV', 'TV', 'YE', 'AF', 'BD', 'CA', 'CX', 'FI', 'GR', 'IQ', 'KY', 'MF', 'MY', 'PF', 'SA', 'SX', 'TW', 'YT', 'AG', 'BE', 'CC', 'CY', 'FJ', 'GS', 'IR', 'KZ', 'MG', 'MZ', 'PG', 'SB', 'SY', 'TZ', 'ZA', 'AI', 'BF', 'CD', 'CZ', 'FK', 'GT', 'IS', 'LA', 'MH', 'NA', 'PH', 'SC', 'SZ', 'UA', 'ZM', 'AL', 'BG', 'CF', 'DE', 'FM', 'GU', 'IT', 'LB', 'MK', 'NC', 'PK', 'SD', 'TC', 'UG', 'ZW', 'AM', 'BH', 'CG', 'DJ', 'FO', 'GW', 'JE', 'LC', 'ML', 'NE', 'PL', 'SE', 'TD', 'US', 'AN', 'BI', 'CH', 'DK', 'FR', 'GY', 'JM', 'LI', 'MM', 'NF', 'PN', 'SG', 'TF', 'UY', 'AO', 'BJ', 'CI', 'DM', 'GA', 'HK', 'JO', 'LK', 'MN', 'NG', 'PR', 'SH', 'TG', 'UZ', 'AQ', 'BL', 'CK', 'DO', 'GB', 'HN', 'JP', 'LR', 'MO', 'NI', 'PS', 'SI', 'TH', 'VA', 'AR', 'BM', 'CL', 'DZ', 'GD', 'HR', 'KE', 'LS', 'MP', 'NL', 'PT', 'SK', 'TJ', 'VC', 'AS', 'BN', 'CM', 'EC', 'GE', 'HT', 'KG', 'LT', 'MQ', 'NO', 'PW', 'SL', 'TK', 'VE', 'AT', 'BO', 'CN', 'EE', 'GG', 'HU', 'KH', 'LU', 'MR', 'NP', 'PY', 'SM', 'TL', 'VG', 'AU', 'BR', 'CO', 'EG', 'GH', 'IC', 'KI', 'LV', 'MS', 'NR', 'QA', 'SN', 'TM', 'VI', 'AW', 'BS', 'CR', 'EH', 'GI', 'ID', 'KM', 'LY', 'MT', 'NU', 'RE', 'SO', 'TN', 'VN', 'AX', 'BT', 'CT', 'ER', 'GL', 'IE', 'KN', 'MA', 'MU', 'NZ', 'RO', 'SR', 'TO', 'VU', 'AZ', 'BW', 'CU', 'ES', 'GM', 'IL', 'KP', 'MC', 'MV', 'OM', 'RS', 'SS', 'TR', 'WF'];
+let currentIPv4 = null;
+let currentIPv6 = null;
+const countriesSupported = ['AD', 'BA', 'BY', 'CV', 'ET', 'GN', 'IM', 'KR', 'MD', 'MW', 'PA', 'RU', 'ST', 'TT', 'WS', 'AE', 'BB', 'BZ', 'CW', 'EU', 'GQ', 'IN', 'KW', 'ME', 'MX', 'PE', 'RW', 'SV', 'TV', 'YE', 'AF', 'BD', 'CA', 'CX', 'FI', 'GR', 'IQ', 'KY', 'MF', 'MY', 'PF', 'SA', 'SX', 'TW', 'YT', 'AG', 'BE', 'CC', 'CY', 'FJ', 'GS', 'IR', 'KZ', 'MG', 'MZ', 'PG', 'SB', 'SY', 'TZ', 'ZA', 'AI', 'BF', 'CD', 'CZ', 'FK', 'GT', 'IS', 'LA', 'MH', 'NA', 'PH', 'SC', 'SZ', 'UA', 'ZM', 'AL', 'BG', 'CF', 'DE', 'FM', 'GU', 'IS', 'IT', 'LB', 'MK', 'NC', 'PK', 'SD', 'TC', 'UG', 'ZW', 'AM', 'BH', 'CG', 'DJ', 'FO', 'GW', 'JE', 'LC', 'ML', 'NE', 'PL', 'SE', 'TD', 'US', 'AN', 'BI', 'CH', 'DK', 'FR', 'GY', 'JM', 'LI', 'MM', 'NF', 'PN', 'SG', 'TF', 'UY', 'AO', 'BJ', 'CI', 'DM', 'GA', 'HK', 'JO', 'LK', 'MN', 'NG', 'PR', 'SH', 'TG', 'UZ', 'AQ', 'BL', 'CK', 'DO', 'GB', 'HN', 'JP', 'LR', 'MO', 'NI', 'PS', 'SI', 'TH', 'VA', 'AR', 'BM', 'CL', 'DZ', 'GD', 'HR', 'KE', 'LS', 'MP', 'NL', 'PT', 'SK', 'TJ', 'VC', 'AS', 'BN', 'CM', 'EC', 'GE', 'HT', 'KG', 'LT', 'MQ', 'NO', 'PW', 'SL', 'TK', 'VE', 'AT', 'BO', 'CN', 'EE', 'GG', 'HU', 'KH', 'LU', 'MR', 'NP', 'PY', 'SM', 'TL', 'VG', 'AU', 'BR', 'CO', 'EG', 'GH', 'IC', 'KI', 'LV', 'MS', 'NR', 'QA', 'SN', 'TM', 'VI', 'AW', 'BS', 'CR', 'EH', 'GI', 'ID', 'KM', 'LY', 'MT', 'NU', 'RE', 'SO', 'TN', 'VN', 'AX', 'BT', 'CT', 'ER', 'GL', 'IE', 'KN', 'MA', 'MU', 'NZ', 'RO', 'SR', 'TO', 'VU', 'AZ', 'BW', 'CU', 'ES', 'GM', 'IL', 'KP', 'MC', 'MV', 'OM', 'RS', 'SS', 'TR', 'WF'];
 const checkInterval = 3550;
+
+function updateTitle() {
+    let parts = [];
+    if (currentIPv4) parts.push('IPv4: ' + currentIPv4);
+    if (currentIPv6) parts.push('IPv6: ' + currentIPv6);
+    chrome.action.setTitle({ title: parts.length ? parts.join('\n') : 'IP Address & Geolocation' });
+}
 
 async function setBadgeText(text) {
     let showText = await lp.isSet(KEY_SETTINGS_SHOW_TEXT) ? await lp.get(KEY_SETTINGS_SHOW_TEXT) : true;
@@ -67,11 +76,12 @@ async function checkForLocationChange(geoLocation, ipv6) {
 async function fetchGeoLocation() {
     var badgeIndicator = await lp.isSet(KEY_SETTINGS_COUNTRY_BADGE) ? await lp.get(KEY_SETTINGS_COUNTRY_BADGE) : 'auto';
 
-
     var geoLocate = new GeoLocation();
     geoLocate.fetch({
         success: function () {
             ipv4Error = false;
+            currentIPv4 = geoLocate.get('geoLocation').ipAddress || null;
+            updateTitle();
             checkForLocationChange(geoLocate, false);
             if (badgeIndicator === 'ipv4' || badgeIndicator === 'auto') {
                 var country_code = geoLocate.get('geoLocation').countryCode ? geoLocate.get('geoLocation').countryCode : 'ERR';
@@ -92,6 +102,8 @@ async function fetchGeoLocation() {
     var geoLocate6 = new GeoLocation6();
     geoLocate6.fetch({
         success: function () {
+            currentIPv6 = geoLocate6.get('geoLocation').ipAddress || null;
+            updateTitle();
             checkForLocationChange(geoLocate6, true);
             if (badgeIndicator === 'ipv6' || (badgeIndicator === 'auto' && ipv4Error)) {
                 var country_code = geoLocate6.get('geoLocation').countryCode ? geoLocate6.get('geoLocation').countryCode : 'ERR';
@@ -107,7 +119,6 @@ async function fetchGeoLocation() {
         },
         timeout: checkInterval - 50
     });
-
 }
 
 function showChromeNotification(id, title, message, contextMessage, callback) {
@@ -118,7 +129,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.method == "refresh") {
         fetchGeoLocation().then(() => { sendResponse({ data: 'refreshed' }); });
     }
-    return true; // indicates we will send a response asynchronously
+    return true;
 });
 
 setBadgeColor('#000000');
@@ -151,7 +162,6 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 chrome.runtime.onSuspend.addListener(function () {
     stopInterval();
 });
-
 
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === 'checkIntervalAlarm') {
