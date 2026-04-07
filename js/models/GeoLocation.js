821f487b077d18ccd1fa49024334347c83d77614
Zero <183767710+AbsoluteXYZero@users.noreply.github.com>
2026-04-07 19:39:42 -0400

    Add files via upload

--- js/models/GeoLocation.js
+++ js/models/GeoLocation.js
@@ -1,62 +1,5 @@
 const APIS = [
     {
-        url: 'https://ipinfo.io/json',
-        normalize: (d) => ({
-            browser: { userAgent: navigator.userAgent },
-            geoLocation: {
-                ipAddress: d.ip || '',
-                countryCode: d.country || '',
-                countryName: d.country || '',
-                city: d.city || '',
-                state: d.region || '',
-                stateCode: '',
-                continent: '',
-                continentCode: '',
-                timezone: d.timezone || '',
-                latitude: d.loc ? parseFloat(d.loc.split(',')[0]) : 0,
-                longitude: d.loc ? parseFloat(d.loc.split(',')[1]) : 0
-            }
-        })
-    },
-    {
-        url: 'https://ipapi.co/json/',
-        normalize: (d) => ({
-            browser: { userAgent: navigator.userAgent },
-            geoLocation: {
-                ipAddress: d.ip || '',
-                countryCode: d.country_code || '',
-                countryName: d.country_name || '',
-                city: d.city || '',
-                state: d.region || '',
-                stateCode: d.region_code || '',
-                continent: '',
-                continentCode: d.continent_code || '',
-                timezone: d.timezone || '',
-                latitude: d.latitude || 0,
-                longitude: d.longitude || 0
-            }
-        })
-    },
-    {
-        url: 'https://ipwho.is/',
-        normalize: (d) => ({
-            browser: { userAgent: navigator.userAgent },
-            geoLocation: {
-                ipAddress: d.ip || '',
-                countryCode: d.country_code || '',
-                countryName: d.country || '',
-                city: d.city || '',
-                state: d.region || '',
-                stateCode: d.region_code || '',
-                continent: d.continent || '',
-                continentCode: d.continent_code || '',
-                timezone: d.timezone?.id || '',
-                latitude: d.latitude || 0,
-                longitude: d.longitude || 0
-            }
-        })
-    },
-    {
         url: 'https://freeipapi.com/api/json',
         normalize: (d) => ({
             browser: { userAgent: navigator.userAgent },
@@ -72,6 +15,25 @@ const APIS = [
                 timezone: Array.isArray(d.timeZones) ? d.timeZones[0] : '',
                 latitude: d.latitude || 0,
                 longitude: d.longitude || 0
+            }
+        })
+    },
+    {
+        url: 'https://ipinfo.io/json',
+        normalize: (d) => ({
+            browser: { userAgent: navigator.userAgent },
+            geoLocation: {
+                ipAddress: d.ip || '',
+                countryCode: d.country || '',
+                countryName: d.country || '',
+                city: d.city || '',
+                state: d.region || '',
+                stateCode: '',
+                continent: '',
+                continentCode: '',
+                timezone: d.timezone || '',
+                latitude: d.loc ? parseFloat(d.loc.split(',')[0]) : 0,
+                longitude: d.loc ? parseFloat(d.loc.split(',')[1]) : 0
             }
         })
     }
@@ -143,20 +105,7 @@ class GeoLocation6 {
         };
     }
 
-    get(key) {
-        return this.data[key];
-    }
-
-    fetch(options = {}) {
-        fetchWithFallback(options).then(data => {
-            if (data) {
-                this.data = data;
-                if (options.success) options.success();
-            }
-        });
-    }
-
-    toJSON() {
-        return JSON.parse(JSON.stringify(this.data));
-    }
+    get(key) { return this.data[key]; }
+    fetch(options = {}) { if (options.error) options.error(); }
+    toJSON() { return JSON.parse(JSON.stringify(this.data)); }
 }
